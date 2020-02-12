c4b9245b8d6db3a662e5e9d075e15d1308650c31
max-student <max-student@maxtrain.com>
2020-02-12 10:06:43 -0500

    Add project files.

---
+++ PigDice/PigDiceGame.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PigDice {
+    public class PigDiceGame {
+
+        private Random rnd = new Random(DateTime.Now.Millisecond);
+
+        public int RollTheDie() {
+            return rnd.Next(1, 7);
+        }
+
+        public int PigDice() {
+
+            var score = 0;
+            var ROLL = RollTheDie();
+            while (ROLL != 1) {
+                score += ROLL;
+                ROLL = RollTheDie();
+            }
+            return score;
+        }
+
+        public void PLAY() {
+            var WorldRecordScore = 450;
+            var highScore = 0;
+            var GamesToPlay = 100000000;
+            while(GamesToPlay > 0 || highScore < WorldRecordScore) {
+                var score = PigDice();
+                if (score > highScore) {
+                    highScore = score;
+                    Console.WriteLine($"New highscore is {score} ");
+                }
+
+                GamesToPlay--;
+            }
+            Console.WriteLine($"New WW Score is {highScore}");
+            
+
+        }
+
+    }
+}
---
+++ PigDice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PigDice {
+    class Program {
+        static void Main(string[] args) {
+
+            var pigDice = new PigDiceGame();
+            pigDice.PLAY();
+            
+        }
+    }
+}
